d76d02eb8181e4c541aab9d8de3156033d15e32c
Christopher Lutz <chrisblutz@frontier.com>
2019-11-02 18:40:23 -0400

    Update determind.py

--- determind.py
+++ determind.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
