cf678b043351e19dbf2cf9f919e7ce2581b2ab12
Christopher Lutz <chrisblutz@frontier.com>
2019-11-02 18:55:59 -0400

    Add favicon

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from app import app
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'images/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 def home():
     return render_template('home.html')
